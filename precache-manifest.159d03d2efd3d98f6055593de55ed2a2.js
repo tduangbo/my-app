4d08b440718a24aeb53236cfe7d28bf807cbc427
tduangbo <tduangbo@asu.edu>
2019-02-18 23:37:27 -0800

    Updates

---
+++ precache-manifest.159d03d2efd3d98f6055593de55ed2a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "19dce88498223aba175e",
+    "url": "/my-app/static/js/main.19dce884.chunk.js"
+  },
+  {
+    "revision": "4ea9a7e474039d70994e",
+    "url": "/my-app/static/js/2.4ea9a7e4.chunk.js"
+  },
+  {
+    "revision": "19dce88498223aba175e",
+    "url": "/my-app/static/css/main.203ed2a5.chunk.css"
+  },
+  {
+    "revision": "4ea9a7e474039d70994e",
+    "url": "/my-app/static/css/2.95d9a2b0.chunk.css"
+  },
+  {
+    "revision": "a17ad4fcd5043d7955da1f290ea9f08c",
+    "url": "/my-app/index.html"
+  }
+];
